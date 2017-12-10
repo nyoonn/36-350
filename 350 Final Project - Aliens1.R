80bb5a840d04c32ae0c857d38ee117b73760e91c
nyoonn <33400992+nyoonn@users.noreply.github.com>
2017-12-10 14:34:02 -0500

    Add files via upload

---
+++ 350 Final Project - Aliens1.R
@@ -0,0 +1,8 @@
+
+
+cat("Hello Aliens, nice to finally meet you. 
+    My name is Nathan Yoon.
+    How is space travel? 
+    Tell me about your purpose here on Earth. 
+    What is the structure of your government?
+    What is the meaning of life?")
